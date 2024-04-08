ab484bbccbd76b1d5b812c21ca5334ea8193f5b6
Miku <85927933+HatsuneMikuBotSup@users.noreply.github.com>
2024-04-08 12:38:54 +0200

    improved start script

--- start.sh
+++ start.sh
@@ -1,4 +1,14 @@
-git pull origin main --force
-chmod +x start.sh
-npm run build
-node .
+#!/bin/bash
+while true; do
+    # Pull changes from the remote repository and force overwrite local changes
+    git pull origin main --force
+
+    # Ensure the script has execute permissions next time
+    chmod +x start.sh
+
+    # Run the build script
+    npm run build
+
+    # Start your Node.js application
+    node .
+done
