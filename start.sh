aab0a0bb0c063a8af577640f4ce06f656c2c9502
Miku <85927933+HatsuneMikuBotSup@users.noreply.github.com>
2024-04-08 12:46:36 +0200

    better start script

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 while true; do
+    # Reset any local changes and index to match the latest commit on the main branch
+    git reset --hard origin/main
+
     # Pull changes from the remote repository and force overwrite local changes
     git pull origin main --force
 
-    # Ensure the script has execute permissions next time
+    # Ensure the script has execute permissions
     chmod +x start.sh
 
     # Run the build script
@@ -11,4 +15,8 @@ while true; do
 
     # Start your Node.js application
     node .
+
+    # Sleep for a desired duration before repeating the loop
+    # This example sleeps for 1 minute (60 seconds)
+    sleep 60
 done
